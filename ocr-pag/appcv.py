f45bdd0e771268b94d46f6003f003357b26b33f4
joao aguiar <joao_aguiarcosta@hotmail.com>
2025-03-14 13:10:20 -0300

    finding by line v1.5

--- ocr-pag/appcv.py
+++ ocr-pag/appcv.py
@@ -2,15 +2,11 @@ import cv2
 import pytesseract
 import re
 
-
 image = cv2.imread("./src/caca_palavras2.jpg")
 
-#image = cv2.bitwise_not(image, cv2.COLOR_BGR2GRAY)
-
-contrast = 2
-brightness = 3
-
-image = cv2.convertScaleAbs(image, contrast, brightness)
+image = cv2.bitwise_not(image,cv2.COLOR_BAYER_BG2BGR)
+#image = cv2.convertScaleAbs(image, contrast, brightness)
+#image = cv2.bilateralFilter(image,9,75,75)
 
 lines = 7
 columns = 7
@@ -30,18 +26,41 @@ cv2.imshow("caca_palavras", image)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
 
+
+
 for x in range(lines):
 
     left = 0
     right = width.__round__()
     
     letters = []
+    
+    #image = cv2.bitwise_not(image,cv2.COLOR_BAYER_BG2BGR)
+    cropped_image = image[top:bottom, left:x_axis]
+    
+    contrast = 5
+    brightness = 5
+    
+    cropped_image = cv2.convertScaleAbs(cropped_image, contrast, brightness)
+    cropped_image = cv2.bilateralFilter(cropped_image,9,75,75)
 
-    for y in range(columns):
+
+
+    customConfig = '--oem 3 --psm 6'
+    data_line = pytesseract.image_to_string(cropped_image, config=customConfig)
+    data_line = re.sub(r'[^A-Z]', '', data_line)
+    data_line = data_line.replace("\n", "")
+    
+    data[0].append(data_line)
+    data_line = ""
+    cv2.imshow("line" + str(x), cropped_image)
+    cv2.waitKey(0)
+
+    """     for y in range(columns):
         cropped_image = image[top:bottom, left:right]
 
-        cropperd_image = cv2.bilateralFilter(cropped_image,9,75,75)
-        
+        cropped_image = cv2.bitwise_not(cropped_image,cv2.COLOR_BAYER_BG2BGR)
+
         
         #configuração do pytesseract e transformando em string
         customConfig = '--oem 3 --psm 6'
@@ -49,17 +68,17 @@ for x in range(lines):
         letter = re.sub(r'[^A-Z]', '', letter)
         letter = letter.replace("\n", "")
         
-        """ if(x==2 and y==3):
+        if(x==6 and y==0):
             cv2.imshow("3letra", cropped_image)
-            cv2.waitKey(0) """
+            cv2.waitKey(0)
         
         letters.append(letter)
         
         left = left + width
-        right = right + width
+        right = right + width """
         
     bottom = bottom + height
     top = top + height
-    data[0].append("".join(letters))
+    #data[0].append("".join(letters))
     
 print(data)
