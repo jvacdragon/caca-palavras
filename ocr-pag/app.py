98f141b6fbacac8b9b590640579a1f2df00d0c90
joao aguiar <joao_aguiarcosta@hotmail.com>
2025-03-14 00:59:22 -0300

    identificando letra por letra

--- ocr-pag/app.py
+++ ocr-pag/app.py
@@ -8,27 +8,53 @@ import cv2
 import numpy as np
 import re
 
-# matriz (19,12)
-image = Image.open("./src/caca_palavras.jpg")
+image = Image.open("./src/caca_palavras2.jpg")
 
-print(image.getbbox())
+image = ImageEnhance.Contrast(image)
+image = image.enhance(2)
+
 image_dimensions = [image.getbbox()[2], image.getbbox()[3]]
-print(image_dimensions)
+dimensao_caca_palavra = [7, 7]
 
-image.show()
+arrData = []
 
-size_h = image_dimensions[1]/12
-size_w = image_dimensions[0]
+#image.show()
+
+size_h = image_dimensions[1]/dimensao_caca_palavra[1]
+size_w = image_dimensions[0]/dimensao_caca_palavra[0]
 
 top = 0
 bottom = size_h
 
-for x in range(12):
+for x in range(2):#dimensao_caca_palavra[1]):
 
-    cropped_image = image.crop((0, top, size_w, bottom))
-    # image = image.crop((0, 0, image_dimensions[0], image_dimensions[1]/12))
+    left = 0
+    right = size_w
+    
+    line = []
+    for y in range(dimensao_caca_palavra[0]):
+        
+        cropped_image = image.crop((left, top, right, bottom))
+    
+        #configuração do pytesseract e transformando em string
+        customConfig = '--oem 3 --psm 6'
+        letra = pytesseract.image_to_string(cropped_image, config=customConfig)
+        
+        letra = re.sub(r'[^a-zA-Z\s]', '', letra)
+        letra = letra.replace("\n", "")
+        
+        line.append(letra)
+        
+        left = left + size_w
+        right = right + size_w
 
-    cropped_image.show()
-
+    
+    line = "".join(line)
+    arrData.append(line)
+    print(line)
+    line = ""
     top = top + size_h
     bottom = bottom + size_h
+
+print(arrData)
+image.show()
