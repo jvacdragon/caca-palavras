394642e8cd5468198dcdeff8fd3d2bd9a87cf45b
joao aguiar <joao_aguiarcosta@hotmail.com>
2025-03-20 13:46:08 -0300

    refatoracao

--- ocr-pag/app.py
+++ ocr-pag/app.py
@@ -2,34 +2,24 @@ import cv2
 import pytesseract
 import re
 
-original_image = cv2.imread("./src/caca_palavras3.png")
+from test import cut
 
-gray = cv2.cvtColor(original_image, cv2.COLOR_BGR2GRAY)
-cv2.imshow("gray", original_image)
+original_image = cv2.imread("./src/caca_palavras.jpg")
 
+lines = 7
+columns = 7
+
+#original_image = cv2.resize(original_image, None, fx= 0.8, fy=0.8, interpolation=cv2.INTER_AREA)
+
+gray, top, bottom, x_axis, width, height = cut(original_image, lines, columns)
 val, thresh = cv2.threshold(gray, 200, 255, cv2.THRESH_BINARY)
-cv2.imshow("thresh", thresh)
-
-print(val)
-
-lines = 11
-columns = 11
-
-x_axis = original_image.shape[0]
-y_axis = original_image.shape[1]
-
-height = (y_axis/lines).__round__()
-width = (x_axis/columns).__round__()
-
-top = 0
-bottom = height.__round__()
 
 data = [[], []]
 
 cv2.imshow("caca_palavras", thresh)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
-
+    
 
 for x in range(lines):
 
@@ -37,35 +27,38 @@ for x in range(lines):
     right = width.__round__()
 
     letters = []
-
-    """ cropped_image = image[top:bottom, left:x_axis] """
     cropped_image = thresh[top:bottom, left:x_axis]
 
-    """ contrast = 0
-    brightness = 5
-    
-    cropped_image = cv2.convertScaleAbs(cropped_image, contrast, brightness)
-    cropped_image = cv2.bilateralFilter(cropped_image,9,75,75) """
 
     customConfig = '--oem 3 --psm 6'
     data_line = pytesseract.image_to_string(cropped_image, config=customConfig)
     data_line = re.sub(r'[^A-Z]', '', data_line)
     data_line = data_line.replace("\n", "")
 
-    if (data_line.__len__() > columns):
-        val, thresh = cv2.threshold(gray, 220, 255, cv2.THRESH_BINARY)
-        cropped_image = thresh[top:bottom, left:x_axis]
+    if (data_line.__len__() == columns+1):
+        print(data_line.__len__())
+        val, thresh_replace = cv2.threshold(gray, 220, 255, cv2.THRESH_BINARY)
+
+        cropped_image = thresh_replace[top:bottom, left:x_axis]
         customConfig = '--oem 3 --psm 6'
         data_line = pytesseract.image_to_string(
-            cropped_image, config=customConfig)
+        cropped_image, config=customConfig)
         data_line = re.sub(r'[^A-Z]', '', data_line)
         data_line = data_line.replace("\n", "")
+        
+    """ if(data_line.__len__() >= (columns+2)):
+        amplified_image = cv2.resize(cropped_image, None, fx=0.8, fy=0.8, interpolation=cv2.INTER_AREA)
+        cv2.imshow("amplified", amplified_image)
+        cv2.waitKey(0)
+        #cropped_image = amplified_image[top:bottom, left:x_axis]
+        customConfig = '--oem 3 --psm 6'
+        data_line = pytesseract.image_to_string(
+        cropped_image, config=customConfig)
+        data_line = re.sub(r'[^A-Z]', '', data_line)
+        data_line = data_line.replace("\n", "") """
 
     data[0].append(data_line)
     data_line = ""
-
-    cv2.imshow("line" + str(x), cropped_image)
-    cv2.waitKey(0)
 
     bottom = bottom + height
     top = top + height
---
+++ ocr-pag/test.py
@@ -0,0 +1,17 @@
+import cv2
+
+
+def cut(image, lines, columns):
+    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+    x_axis = image.shape[0]
+    y_axis = image.shape[1]
+    height = (y_axis/lines).__round__()
+
+
+    width = (x_axis/columns).__round__()
+
+    top = 0
+    bottom = height.__round__()
+    
+    return(gray, top, bottom, x_axis, width, height)
