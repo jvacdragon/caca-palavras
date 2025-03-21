97f1516b33bc092668f02098f8ceebf1fdd4e893
joao aguiar <joao_aguiarcosta@hotmail.com>
2025-03-20 23:12:59 -0300

    corrigindo erro do primeiro if

--- ocr-pag/app.py
+++ ocr-pag/app.py
@@ -33,16 +33,13 @@ for x in range(lines):
     data_line = pytesseract.image_to_string(cropped_image, config=customConfig)
     data_line = re.sub(r'[^A-Z]', '', data_line)
     data_line = data_line.replace("\n", "")
-
-    
         
-    print(x)
     if(data_line.__len__() >= (columns+2)):
         resized_image = cv2.resize(original_image, None, fx=0.8, fy=0.8, interpolation=cv2.INTER_AREA)
         
         new_gray, new_top, new_bottom, new_x_axis, new_width, new_height = cut(resized_image, lines, columns)
         
-        val, thresh = cv2.threshold(new_gray, 200, 255, cv2.THRESH_BINARY)
+        val, new_thresh = cv2.threshold(new_gray, 200, 255, cv2.THRESH_BINARY)
 
         new_left = 0
         new_right = new_width.__round__()
@@ -51,23 +48,17 @@ for x in range(lines):
             new_bottom = (new_bottom + new_height*x)
             new_top = (new_top + new_height*x)
                 
-        cropped_image = thresh[new_top:new_bottom, new_left:new_x_axis]
+        new_cropped_image = new_thresh[new_top:new_bottom, new_left:new_x_axis]
         
-        cv2.imshow("amplified" + str(x), cropped_image)
-        cv2.waitKey(0)
-        #cropped_image = resized_image[top:bottom, left:x_axis]
         customConfig = '--oem 3 --psm 6'
         data_line = pytesseract.image_to_string(
-        cropped_image, config=customConfig)
+        new_cropped_image, config=customConfig)
         data_line = re.sub(r'[^A-Z]', '', data_line)
         data_line = data_line.replace("\n", "")
 
-    if (data_line.__len__() == columns+1 or data_line.__len__() < columns):
+    if (data_line.__len__() == (columns+1) or data_line.__len__() < columns):
         val, thresh_replace = cv2.threshold(gray, 220, 255, cv2.THRESH_BINARY)
         cropped_image = thresh_replace[top:bottom, left:x_axis]
-        
-        cv2.imshow("teste", cropped_image)
-        cv2.waitKey(0)
         
         customConfig = '--oem 3 --psm 6'
         data_line = pytesseract.image_to_string(
