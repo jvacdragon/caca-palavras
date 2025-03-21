819fd5c03a217adc08f5c4ecd3dbbc81add9deac
joao aguiar <joao_aguiarcosta@hotmail.com>
2025-03-21 11:43:38 -0300

    aplicacao concluida

--- ocr-pag/app.py
+++ ocr-pag/app.py
@@ -1,40 +1,43 @@
 import cv2
-import pytesseract
-import re
 
-from helpers import cut
 
-original_image = cv2.imread("./src/caca_palavras.jpg")
+from helpers import cut, find, process_image_text
+
+#passando parametros para que a funcione: caminho para imagem, quantidade de linhas do caça palavras quantidade de colunas do caça palavras e um array com as palavras a serem encontradas
+image_path = "./src/caca_palavras2.jpg"
+find_elements = ["WTF", "NEO", "BLEED", "RUSU"]
 lines = 7
 columns = 7
+#leitura inicial da imagem
+original_image = cv2.imread(image_path)
 
 def main(original_image, lines, columns):
-    gray, top, bottom, x_axis, width, height = cut(original_image, lines, columns)
-    val, thresh = cv2.threshold(gray, 200, 255, cv2.THRESH_BINARY)
+    gray, top, bottom, x_axis, height = cut(original_image, lines)
+    _, thresh = cv2.threshold(gray, 200, 255, cv2.THRESH_BINARY)
 
     data = [[], []]
 
     for x in range(lines):
 
         left = 0
-        right = width.__round__()
 
+        #cortando a imagem
         cropped_image = thresh[top:bottom, left:x_axis]
 
+        #fazendo leitura inicial da imagem cortada para identificação das letras
         customConfig = '--oem 3 --psm 6'
-        data_line = pytesseract.image_to_string(cropped_image, config=customConfig)
-        data_line = re.sub(r'[^A-Z]', '', data_line)
-        data_line = data_line.replace("\n", "")
+        
+        data_line = process_image_text(cropped_image, customConfig)
             
+        #mudando diminuindo o zoom caso tenham mais de 1 letra a mais encontradas na linha para melhor identificação delas
         if(len(data_line) >= (columns+2)):
             resized_image = cv2.resize(original_image, None, fx=0.8, fy=0.8, interpolation=cv2.INTER_AREA)
             
-            new_gray, new_top, new_bottom, new_x_axis, new_width, new_height = cut(resized_image, lines, columns)
+            new_gray, new_top, new_bottom, new_x_axis, new_height = cut(resized_image, lines, columns)
             
-            val, new_thresh = cv2.threshold(new_gray, 200, 255, cv2.THRESH_BINARY)
+            _, new_thresh = cv2.threshold(new_gray, 200, 255, cv2.THRESH_BINARY)
 
             new_left = 0
-            new_right = new_width.__round__()
             
             if(x>=1):
                 new_bottom = (new_bottom + new_height*x)
@@ -42,32 +45,31 @@ def main(original_image, lines, columns):
                     
             new_cropped_image = new_thresh[new_top:new_bottom, new_left:new_x_axis]
             
-            data_line = pytesseract.image_to_string(
-            new_cropped_image, config=customConfig)
-            data_line = re.sub(r'[^A-Z]', '', data_line)
-            data_line = data_line.replace("\n", "")
+            data_line = process_image_text(new_cropped_image, customConfig)
 
+        #mudando arbitrariamente o threshold da imagem caso sejam identificadas menos letras ou apenas uma letra a mais do que deveria
         if (len(data_line) == (columns+1) or len(data_line) < columns):
-            val, thresh_replace = cv2.threshold(gray, 220, 255, cv2.THRESH_BINARY)
+            _, thresh_replace = cv2.threshold(gray, 220, 255, cv2.THRESH_BINARY)
             cropped_image = thresh_replace[top:bottom, left:x_axis]
             
-            data_line = pytesseract.image_to_string(
-            cropped_image, config=customConfig)
-            data_line = re.sub(r'[^A-Z]', '', data_line)
-            data_line = data_line.replace("\n", "")
+            data_line = process_image_text(cropped_image, customConfig)
         
         data[0].append(data_line)
         
+        #adicionando colunas no array baseado ja nas linhas escaneadas
         for i, letter in enumerate(data[0][x]):
             if(len(data[1]) <= i):
                 data[1].append(letter)
             else:
                 data[1][i] = data[1][i] + letter
-            
+          
+        
         data_line = ""
+        
+        #calculo para a altura da próxima linha a ser lida
         bottom = bottom + height
         top = top + height
-        
-    return data
+            
+    return find(data, find_elements)
 
 print(main(original_image, lines, columns))
--- ocr-pag/helpers.py
+++ ocr-pag/helpers.py
@@ -1,16 +1,54 @@
 import cv2
+import pytesseract
+import re
 
-
-def cut(image, lines, columns):
+#Cortando a imagem para leitura de linhas do caça palavras
+def cut(image, lines):
     gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
     x_axis = image.shape[0]
     y_axis = image.shape[1]
-    height = (y_axis/lines).__round__()
-    width = (x_axis/columns).__round__()
+    height = round((y_axis/lines))
 
     top = 0
-    bottom = height.__round__()
+    bottom = round(height)
     
-    return(gray, top, bottom, x_axis, width, height)
+    return(gray, top, bottom, x_axis, height)
 
+#achando a palavra no caça palavras e indicando sua localização baseada na coluna e index inicial
+def find(arr, look_for):    
+    words_finded = []
+    
+
+    for key in look_for:
+            for i, line in enumerate(arr[0]):
+                
+                match = re.search(key, line)
+                
+                if(match):
+                    word = match.group()
+                    word_line = i + 1
+                    word_index = match.start() + 1
+                    
+                    words_finded.append({"word": word, "line": word_line, "index": word_index})
+                    break
+            
+            for i, column in enumerate(arr[1]):
+                
+                match = re.search(key, column)
+                
+                if(match):
+                    word = match.group()
+                    word_column = i + 1
+                    word_index = match.start() + 1
+                    
+                    words_finded.append({"word": word, "column": word_column, "index": word_index})
+                    break
+                    
+    return words_finded
+
+#procedimento padrão para processamento dos textos da imagem
+def process_image_text(image, customConfig):
+        data_line = pytesseract.image_to_string(image, config=customConfig)
+        data_line = re.sub(r'[^A-Z]', '', data_line)
+        return data_line.replace("\n", "")
